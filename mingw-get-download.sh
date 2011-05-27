bca2b9dcda5518698bb7eda950898571688434ab
Sebastian Schuberth <sschuberth@gmail.com>
2011-05-27 18:19:12 +0200

    Install mingw in a directory below the msys root
    And perform the initial download of the package list.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- mingw-get-download.sh
+++ mingw-get-download.sh
@@ -8,4 +8,22 @@ link=$(curl -s http://sourceforge.net/api/file/index/project-id/2435/mtime/desc/
      sed -nr "s/<link>(.+mingw-get-[0-9]+\.[0-9]+-mingw32-.+-bin\.tar.+)<\/link>/\1/p" |
      head -1)
 
-mkdir -p mingw-get && curl -L $link | tar -xzC mingw-get
+mkdir -p root/mingw && cd root/mingw && (
+    curl -L $link | tar -xz
+
+    # Install mingw in a directory below the msys root.
+    cat > var/lib/mingw-get/data/profile.xml << EOF
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<profile project="MinGW" application="mingw-get">
+    <repository uri="http://prdownloads.sourceforge.net/mingw/%F.xml.lzma?download">
+    </repository>
+    <system-map id="default">
+      <sysroot subsystem="mingw32" path="%R" />
+      <sysroot subsystem="msys" path="%R/../" />
+    </system-map>
+</profile>
+EOF
+
+    # Get the list of available packages.
+    bin/mingw-get update
+)
