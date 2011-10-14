33995d5779de05715515222d34a290b269803d8b
Sebastian Schuberth <sschuberth@gmail.com>
2011-10-14 17:37:55 +0200

    Add an error message to the build script if Inno Setup cannot be found
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- build-installer.sh
+++ build-installer.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 path=$(reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Inno Setup 5_is1" | grep InstallLocation | cut -c 34-)
-"$path/ISCC.exe" installer.iss
+iscc="$path/ISCC.exe"
+
+if [ -f $iscc ]; then
+    $iscc installer.iss
+else
+    echo "ERROR: Unable to find an Inno Setup installation."
+    exit 1
+fi
