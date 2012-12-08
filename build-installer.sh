9d09fb9d243bc7a11713e4fd414feed86e4ed8d7
Pat Thoyts <patthoyts@users.sourceforge.net>
2012-12-08 21:30:37 +0100

    Properly handle spaces in the Inno Setup path
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- build-installer.sh
+++ build-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 iscc="c:/msysgit/share/InnoSetup/ISCC.exe"
-if [ ! -f $iscc ]; then
+if [ ! -f "$iscc" ]; then
     path=$(reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Inno Setup 5_is1" | grep InstallLocation | cut -c 34-)
     iscc="$path/ISCC.exe"
 fi
 
-if [ -f $iscc ]; then
-    $iscc "installer\installer.iss"
+if [ -f "$iscc" ]; then
+    "$iscc" "installer\installer.iss"
 else
     echo "ERROR: Unable to find an Inno Setup installation."
     exit 1
