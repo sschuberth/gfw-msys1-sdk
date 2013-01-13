5343ebcb339f3bc4b8aa0e4ed25dfe73e7538c65
Sebastian Schuberth <sschuberth@gmail.com>
2013-01-13 21:22:41 +0100

    Add a script to install / update Inno Setup to the mingwGitDevEnv root
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

---
+++ update-inno-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Find a suitable download tool.
+if [ -f "$(type -p curl)" ]; then
+    download="curl"
+    download_args_rss="-s"
+    download_args="-# -L -o"
+elif [ -f "$(type -p wget)" ]; then
+    download="wget"
+    download_args_rss="-q -O -"
+    download_args="-N -O"
+else
+    echo "ERROR: No suitable download tool found."
+    exit 1
+fi
+
+# Download the most recent Inno Setup version.
+file="is-unicode.exe"
+url="http://www.jrsoftware.org/download.php/$file"
+
+if [ "$download" == "curl" ]; then
+    download_args="$download_args $file -R -z"
+fi
+$download $download_args $file $url
+
+if [ -f $file ]; then
+    # Remove any previous installation.
+    rm -fr root/share/InnoSetup/
+
+    # Silently install Inno Setup below the mingw root.
+    wine=$(type -p wine)
+    if [ $? -eq 0 ]; then
+        $wine $file /verysilent /dir="root\share\InnoSetup" /noicons /tasks="" /portable=1
+    else
+        # See http://www.mingw.org/wiki/Posix_path_conversion.
+        $file //verysilent //dir="root\share\InnoSetup" //noicons //tasks="" //portable=1
+    fi
+
+    # Remove unneeded files from the installation.
+    ( cd root/share/InnoSetup/ && rm -fr Examples/ Compil32.exe isscint.dll )
+
+    # Clean up.
+    rm $file
+fi
