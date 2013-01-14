81014b5b4bd38505f6c519bce09a0ff66e802f17
Sebastian Schuberth <sschuberth@gmail.com>
2013-01-14 11:47:10 +0100

    Use the committed Inno Setup to build the installer
    Also support building the installer via wine along the way.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- build-installer.sh
+++ build-installer.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-iscc="c:/msysgit/share/InnoSetup/ISCC.exe"
+iscc="root/share/InnoSetup/ISCC.exe"
 if [ ! -f "$iscc" ]; then
     path=$(reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Inno Setup 5_is1" 2> /dev/null | grep InstallLocation | cut -c 34-)
     iscc="$path/ISCC.exe"
 fi
 
 if [ -f "$iscc" ]; then
-    "$iscc" "installer\installer.iss"
+    wine=$(type -p wine)
+    $wine $iscc installer/installer.iss
 else
     echo "ERROR: Unable to find an Inno Setup installation."
     exit 1
