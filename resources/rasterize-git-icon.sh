0d5e82bae4a2460bf2ddf5f6b2fdf033096c8068
Sebastian Schuberth <sschuberth@gmail.com>
2013-03-26 13:09:12 +0100

    Write icon images largest to smallest for increased compatibility with Tk
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- resources/rasterize-git-icon.sh
+++ resources/rasterize-git-icon.sh
@@ -6,16 +6,16 @@
 #     http://msdn.microsoft.com/en-us/library/windows/desktop/aa511280.aspx#size
 # for details.
 convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps \
-    \( -clone 0 -resize 16x16 \) \
-    \( -clone 0 -resize 24x24 \) \
-    \( -clone 0 -resize 32x32 \) \
-    \( -clone 0 -resize 48x48 \) \
-    \( -clone 0 -resize 64x64 \) \
     \( -clone 0 -resize 256x256 \) \
-    \( -clone 0 -resize 16x16 -alpha remove -colors 256 \) \
-    \( -clone 0 -resize 24x24 -alpha remove -colors 256 \) \
-    \( -clone 0 -resize 32x32 -alpha remove -colors 256 \) \
-    \( -clone 0 -resize 48x48 -alpha remove -colors 256 \) \
+    \( -clone 0 -resize 64x64 \) \
+    \( -clone 0 -resize 48x48 \) \
+    \( -clone 0 -resize 32x32 \) \
+    \( -clone 0 -resize 24x24 \) \
+    \( -clone 0 -resize 16x16 \) \
     \( -clone 0 -resize 64x64 -alpha remove -colors 256 \) \
+    \( -clone 0 -resize 48x48 -alpha remove -colors 256 \) \
+    \( -clone 0 -resize 32x32 -alpha remove -colors 256 \) \
+    \( -clone 0 -resize 24x24 -alpha remove -colors 256 \) \
+    \( -clone 0 -resize 16x16 -alpha remove -colors 256 \) \
     -delete 0 \
     git.ico
