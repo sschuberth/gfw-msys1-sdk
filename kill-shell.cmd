b79deb07d62a70ae5249fcc9dc83925c9d9cc686
Sebastian Schuberth <sschuberth@gmail.com>
2013-03-30 13:52:10 +0100

    kill-shell: Make the script more readable
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- kill-shell.cmd
+++ kill-shell.cmd
@@ -1,3 +1,8 @@
 @echo off
 
-for /f "tokens=2" %%p in ('wmic process get executablepath^,processid ^| findstr /c:"mingwGitDevEnv\bin\sh.exe"') do taskkill /f /pid %%p
+setlocal
+
+set command="wmic process get executablepath,processid | findstr /c:"mingwGitDevEnv\bin\sh.exe""
+for /f "tokens=2" %%p in ('%command%') do (
+    taskkill /f /pid %%p
+)
