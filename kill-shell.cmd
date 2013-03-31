384482c2977ca66d999b8b61b36cd91766a089dc
Sebastian Schuberth <sschuberth@gmail.com>
2013-03-31 14:24:25 +0200

    kill-shell: Remove the Jenkins test code again
    Rather write it to the Jenkins job configuration.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- kill-shell.cmd
+++ kill-shell.cmd
@@ -4,10 +4,5 @@ setlocal
 
 set command="wmic process get executablepath,processid | findstr /c:"mingwGitDevEnv\bin\sh.exe""
 for /f "tokens=2" %%p in ('%command%') do (
-    taskkill /f /pid 34982349234
-)
-
-if errorlevel 1 (
-    echo An error occurred, forcing the errorlevel to 0.
-    exit /b 0
+    taskkill /f /pid %%p
 )
