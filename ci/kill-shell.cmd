6705bf0c3ba3a85a7d25debc555af2bd4a99ff87
Sebastian Schuberth <sschuberth@gmail.com>
2013-04-05 12:22:07 +0200

    kill-shell: Fix the script for good; it was broken in 999f922
    Turns out Unicode was not an issue, only errorlevel handling of "findstr"
    inside of "for". Also adjust the ignore rules to match .log files in the
    "ci" directory.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- ci/kill-shell.cmd
+++ ci/kill-shell.cmd
@@ -1,10 +1,11 @@
 @echo off
 
-wmic /output:process.log process get executablepath,processid
-
-REM Use "type" to convert Unicode to ASCII text before using "findstr".
-type process.log | findstr /c:"mingwGitDevEnv\bin\sh.exe"
+wmic process get executablepath,processid | findstr /c:"mingwGitDevEnv\bin\sh.exe" > process.log
 if errorlevel 1 (
     echo No shell process found that needs to be terminated.
     exit /b 0
+)
+
+for /f "tokens=2" %%p in (process.log) do (
+    taskkill /f /pid %%p
 )
