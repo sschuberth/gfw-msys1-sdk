09456fa6f5645bbedabefb51aecf330bb1d193c7
Sebastian Schuberth <sschuberth@gmail.com>
2013-04-07 13:55:52 +0200

    kill-shell: Kill child processes manually
    This avoids taskkill returning an error when trying to kill a child
    process that was implicitly killed before.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- ci/kill-shell.cmd
+++ ci/kill-shell.cmd
@@ -1,11 +1,11 @@
 @echo off
 
-wmic process get executablepath,processid | findstr /c:"mingwGitDevEnv\bin\sh.exe" > process.log
+wmic process get executablepath,processid | findstr /c:"mingwGitDevEnv" > process.log
 if errorlevel 1 (
     echo No shell process found that needs to be terminated.
     exit /b 0
 )
 
 for /f "tokens=2" %%p in (process.log) do (
-    taskkill /t /f /pid %%p
+    taskkill /f /pid %%p
 )
