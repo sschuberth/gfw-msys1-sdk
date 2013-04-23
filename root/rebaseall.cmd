a482c151ef8d2edbe68d9934b06fdb6fdd92f251
Sebastian Schuberth <sschuberth@gmail.com>
2013-04-23 15:46:27 +0200

    Add a batch script to rebase all DLLs
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

---
+++ root/rebaseall.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+pushd %~dp0
+ 
+rem Fix permissions for Perl DLLs.
+attrib -r *.dll /s
+
+:retry
+
+rem Rebase DLLs from within dash.
+bin\dash.exe -c "/bin/rebaseall -v"
+if errorlevel 1 (
+    echo.
+    set ANSWER=y
+    set /p ANSWER="Try again? (Y/n) "
+    if /i "!ANSWER!"=="n" goto quit
+    if /i "!ANSWER!"=="no" goto quit
+    echo.
+    goto retry
+)
+
+:quit
+
+popd
