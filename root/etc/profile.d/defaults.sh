aebac8471e0bdcef665883bdaa108d95e2befadb
Sebastian Schuberth <sschuberth@gmail.com>
2013-06-23 10:15:42 +0200

    profile.d: Combine scripts into a single one for faster shell startup
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- root/etc/profile.d/defaults.sh
+++ root/etc/profile.d/defaults.sh
@@ -1,8 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+# Allow to execute *.bat and *.cmd files directly without the need to
+# manually prefix each call with "cmd //c".
+SOURCE=${BASH_SOURCE[0]}
+source $(dirname $SOURCE)/preexec.bash
+
+function preexec () {
+    ext="${1##*.}"
+
+    if [ "$ext" = "bat" ] || [ "$ext" = "cmd" ]; then
+        cmd //c "$1"
+        false
+    fi
+}
+
+preexec_install
+
+# Make vim the default editor as we do not ship vi.
+export EDITOR=vim
 
 # For some reason MSYS converts all Windows environment variables to upper case,
 # but some programs require the proxy environment variables to be in lower case.
-
 if [ -z "$http_proxy" ] && [ -n "$HTTP_PROXY" ]; then
     export http_proxy=$HTTP_PROXY
 fi
@@ -30,3 +48,11 @@ if [ -z "$ftps_proxy" ]; then
         export ftps_proxy=$ftp_proxy
     fi
 fi
+
+# Overload "cd" just for the first use after this function is defined, which would be the
+#     cd "$HOME"
+# call at the end of /etc/profile. With this trick we can start in the root instead of $HOME.
+function cd() {
+    unset -f cd
+    cd /
+}
--- root/etc/profile.d/set-editor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export EDITOR=vim
