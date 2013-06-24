1401a42c4542e60548f151efdedf3434c71f8646
Sebastian Schuberth <sschuberth@gmail.com>
2013-06-24 08:47:33 +0200

    profile.d: Split out start-in-root.sh again, we need this for CI builds
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- root/etc/profile.d/defaults.sh
+++ root/etc/profile.d/defaults.sh
@@ -48,11 +48,3 @@ if [ -z "$ftps_proxy" ]; then
         export ftps_proxy=$ftp_proxy
     fi
 fi
-
-# Overload "cd" just for the first use after this function is defined, which would be the
-#     cd "$HOME"
-# call at the end of /etc/profile. With this trick we can start in the root instead of $HOME.
-function cd() {
-    unset -f cd
-    cd /
-}
