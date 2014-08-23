bf025d5f48c897dc11898377064c8e61b4cde65e
Karsten Blees <blees@dcon.de>
2014-08-23 18:14:16 +0200

    /etc/profile: start in the current working directory
    Do not change the current directory when starting a shell.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- root/etc/profile.d/change_login_dir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Overload "cd" just for the first use after this function is defined, which would be the
-#     cd "$HOME"
-# call at the end of /etc/profile. With this trick we can start in the root instead of $HOME.
-function cd() {
-    unset -f cd
-    [ -n "$LOGIN_DIR" ] && cd "$LOGIN_DIR" || cd /
-}
