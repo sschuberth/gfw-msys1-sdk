fe1a051303915ca61f7db4caad7687dad0ff5f36
Sebastian Schuberth <sschuberth@gmail.com>
2014-10-24 22:54:37 +0200

    Enable the rasterize scripts to create both SDK and Git installer images
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- root/share/resources/rasterize-git-icon.sh
+++ root/share/resources/rasterize-git-icon.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+if [ "$1" = "sdk" ]; then
+    option="-negate"
+    suffix="-sdk"
+fi
+
 # See
 #     http://www.imagemagick.org/Usage/thumbnails/#favicon
 # and
 #     http://msdn.microsoft.com/en-us/library/windows/desktop/aa511280.aspx#size
 # for details.
-convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps -negate \
+convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps $option \
     \( -clone 0 -resize 256x256 \) \
     \( -clone 0 -resize 64x64 \) \
     \( -clone 0 -resize 48x48 \) \
@@ -18,4 +23,4 @@ convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps -negate \
     \( -clone 0 -resize 24x24 -alpha remove -colors 256 \) \
     \( -clone 0 -resize 16x16 -alpha remove -colors 256 \) \
     -delete 0 \
-    git.ico
+    git$suffix.ico
--- root/share/resources/rasterize-git-small.sh
+++ root/share/resources/rasterize-git-small.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps -negate \
+if [ "$1" = "sdk" ]; then
+    option="-negate"
+    suffix="-sdk"
+fi
+
+convert -colorspace sRGB -density 256x256 Git-Icon-1788C.eps $option \
     -resize 55x55 -gravity center -extent 55x58 -alpha off -colors 256 \
-    git-small.bmp
+    git-small$suffix.bmp
