14181acd20cbe3239404264e946141cff4cff081
Sebastian Schuberth <sschuberth@gmail.com>
2014-11-19 11:38:59 +0100

    Add a fallback for "timeout" for pre-Vista Windows versions
    Also get rid of the optional "/t" switch to "timeout", the command is
    already called "timeout" after all.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- root/update-perl-modules.cmd
+++ root/update-perl-modules.cmd
@@ -3,4 +3,11 @@
 set MSYSTEM=MSYS
 bin\sh.exe --login update-perl-modules.sh
 
-timeout /t 5
+set seconds=5
+where timeout > nul 2>&1
+if errorlevel 0 (
+    timeout %seconds%
+) else (
+    echo Waiting for %seconds% seconds...
+    ping -n %seconds% 127.0.0.1 > nul
+)
