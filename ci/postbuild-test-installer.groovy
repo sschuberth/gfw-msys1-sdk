d5468f77b5de8e323d1746c2ace058814076ef02
Sebastian Schuberth <sschuberth@gmail.com>
2015-09-02 21:44:59 +0200

    ci: Store all Jenkins job configurations in a dedicated directory
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- ci/postbuild-test-installer.groovy
+++
@@ -1,8 +0,0 @@
-def workspace = manager.build.workspace.getRemote()
-
-if (manager.contains(new File(workspace + File.separator + "mingw-get.log"), "^.*\\*\\*\\* ERROR \\*\\*\\*.*\$")) {
-    def message = "mingw-get reported an error!"
-    manager.createSummary("error.gif").appendText("<b>" + message + "</b>", false)
-    manager.addErrorBadge(message)
-    manager.buildUnstable()
-}
